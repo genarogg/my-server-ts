f8bb878d611ca73e5f615945cf51f09240f2d7d8
Genaro Gonzalez <genarrogg@gmail.com>
2024-07-21 17:05:12 -0400

    electron install

---
+++ electron.js
@@ -0,0 +1,17 @@
+import { app, BrowserWindow } from 'electron';
+
+const createWindow = () => {
+  // Crea la ventana del navegador.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  // y carga el archivo index.html de la aplicación.
+  win.loadFile('index.html'); // Asegúrate de tener un archivo HTML para cargar o cambia esto para cargar una URL
+};
+
+app.on('ready', createWindow);
