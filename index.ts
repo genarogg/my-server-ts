e8107b77ef9261e7da3691f9a3c1d89d286b667b
Genaro Gonzalez <genarrogg@gmail.com>
2025-01-21 12:52:02 -0400

    prisma

--- index.ts
+++ index.ts
@@ -1,53 +1,46 @@
-// Limpia la consola
-import clear from "console-clear";
-clear();
+import express from 'express';
+import path from 'path';
 
-import express, { Request, Response } from "express";
 
-import { log, path } from "@fn";
-import cors from "cors";
-
-// variables de entorno
-const PORT = process.env.PORT || 4000;
-const CORS_URL = process.env.CORS_URL || "*";
-
-// Crear una instancia de express
 const app = express();
+const PORT = process.env.PORT || 4000;
 
-// Usa cors como middleware
-app.use(cors({ origin: CORS_URL }));
-
-// Configurar el directorio public
 app.use(express.static(path.join(__dirname, "src/public")));
 
 // Configurar EJS como motor de vistas
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "src/views"));
+app.set("views", path.join(__dirname, "src/views"))
 
 // Middleware para analizar el cuerpo de las solicitudes
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//Conexión a la base de datos
-import sequelize from "@db";
+// Conexión a la base de datos
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 
-sequelize.sync({ logging: false }).then(() => {
-  log.cyan("db conectada!");
-});
+prisma.$connect()
+  .then(() => {
+    console.log("db conectada!");
+  })
+  .catch((error) => {
+    console.error("Error al conectar a la db:", error);
+  });
 
 // Importar rutas
 import { inicioRouter } from "@router";
 
 app.use("/", inicioRouter);
 
-// import { sendEmail } from "@email/index";
-
 // Middleware de manejo de errores
-app.use((err: any, req: Request, res: Response, next: Function) => {
+app.use((err: any, req: any, res: any, next: any) => {
   console.error(err.stack);
   res.status(500).send("¡Algo salió mal!");
 });
 
+
 app.listen(PORT, () => {
-  log.green(`El servidor esta corriendo http://localhost:${PORT}`);
+  console.log(`El servidor esta corriendo en http://localhost:${PORT}`);
+
+ 
 });
