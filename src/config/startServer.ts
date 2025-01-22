8ecd231a15ec362c5f4bcbd7a7cf61062ef092eb
Genaro Gonzalez <genarrogg@gmail.com>
2025-01-22 15:38:00 -0400

    dividir logica

--- src/config/startServer.ts
+++ src/config/startServer.ts
@@ -2,13 +2,6 @@ import { log } from "@fn";
 
 const PORT = process.env.PORT || 4000;
 
-const sumar = (a: string | number, b: number): number => {
-    if (typeof a === 'string') {
-        a = parseFloat(a);
-    }
-    return a + b;
-}
-
 const startServer = (app: any) => {
     app.listen(PORT, () => {
         log.success(`El servidor esta corriendo en http://localhost:${PORT}`);
