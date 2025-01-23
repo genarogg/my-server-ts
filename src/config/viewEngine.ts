812b38e5015b585e7a08aff55a79dbafa2c129f2
Genaro Gonzalez <genarrogg@gmail.com>
2025-01-22 21:42:14 -0400

    fix

--- src/config/viewEngine.ts
+++ src/config/viewEngine.ts
@@ -1,8 +1,9 @@
+import { path } from "@fn";
+
 const viewEngine = (app: any) => {
     app.set('view engine', 'jsx');
-    app.set("views", path.join(__dirname, "src/views"))
+    app.set("views", path.join(__dirname, "../views"))
     app.engine('jsx', require('express-react-views').createEngine());
-
 }
 
 export default viewEngine;
