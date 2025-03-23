fa4dd1bd9bfaf872a26a0b12f5fa1084bb06fb60
Genaro Gonzalez <genarrogg@gmail.com>
2025-03-22 23:31:34 -0400

    serverles

--- src/config/startServer.ts
+++ src/config/startServer.ts
@@ -1,12 +1,17 @@
 import { log } from "@fn";
+import serverless from 'serverless-http'; // Importar serverless-http
 
 const PORT = process.env.PORT || 4000;
 
 const startServer = (app: any) => {
-    app.listen(PORT, () => {
-        log.success(`El servidor esta corriendo en http://localhost:${PORT}`);
-        log.info(`Graphql esta corriendo en http://localhost:${PORT}/graphql`);
-    });
+    if (process.env.IS_SERVERLESS) {
+        return serverless(app); // Devolver la aplicación envuelta en serverless-http si es serverless
+    } else {
+        app.listen(PORT, () => {
+            log.success(`El servidor esta corriendo en http://localhost:${PORT}`);
+            log.info(`Graphql esta corriendo en http://localhost:${PORT}/graphql`);
+        });
+    }
 }
 
 export default startServer;
